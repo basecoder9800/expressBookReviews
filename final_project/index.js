03e22912883c65441dc4b230a6cae60e1012e1a1
basecoder9800 <159504225+basecoder9800@users.noreply.github.com>
2024-05-26 19:04:42 -0500

    finished index.js

--- final_project/index.js
+++ final_project/index.js
@@ -4,19 +4,56 @@ const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
+
+let users = []
+//Check if user exists
+const doesExist = (username)=>{
+  let userswithsamename = users.filter((user)=>{
+    return user.username === username
+  });
+  if(userswithsamename.length > 0){
+    return true;
+  } else {
+    return false;
+  }
+}
+//Check for user authentication
+const authenticatedUser = (username,password)=>{
+  let validusers = users.filter((user)=>{
+    return (user.username === username && user.password === password)
+  });
+  if(validusers.length > 0){
+    return true;
+  } else {
+    return false;
+  }
+}
+
 const app = express();
 
 app.use(express.json());
-
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+if(req.session.authorization) { 
+    token = req.session.authorization['accessToken']; 
+    jwt.verify(token, "access",(err,user)=>{ 
+        if(!err){
+            req.user = user;
+            next();
+        }
+        else{
+            return res.status(403).json({message: "User is not authenticated."})
+        }
+     });
+ } else {
+     return res.status(403).json({message: "User is not logged in."})
+ }
 });
- 
-const PORT =5000;
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT,()=>console.log("Server is running at port",PORT));
